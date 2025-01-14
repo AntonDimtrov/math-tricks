637bb7673592d514ec37088ff017039b29129e29
Anton Dimitrov <antonjudo680@gmail.com>
2025-01-14 11:02:46 +0200

    Cell and Player structures

---
+++ index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Cell {
+    char operation;
+    int value;
+    bool isVisited; 
+    int bgColor;    
+};
+
+struct Player {
+    int x, y; 
+    int score;
+};
+
+int main() {
+    
+    return 0;
+}
