0898791aba975dc7d0e14931da254f9429d96116
Anton Dimitrov <antonjudo680@gmail.com>
2025-01-14 12:14:06 +0200

    Display board + set/reset color

--- index.cpp
+++ index.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // colors
 const int DEFAULT_BG = 40;
-const int PLAYER1_BG = 44; 
+const int PLAYER1_BG = 46; 
 const int PLAYER2_BG = 42; 
 const int RED_TEXT = 31;
 const int DEFAULT_TEXT = 37;
@@ -25,6 +25,24 @@ struct Player {
 // global variables
 int rows = 0, cols = 0;
 Player p1, p2;
+
+
+
+void setColor(int bgColor, int textColor = DEFAULT_TEXT) {
+#ifdef _WIN32
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), bgColor | textColor);
+#else
+    cout << "\033[" << bgColor << ";" << textColor << "m";
+#endif
+}
+
+void resetColor() {
+#ifdef _WIN32
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+#else
+    cout << "\033[0m";
+#endif
+}
 
 Cell** initializeBoard(int rows, int cols) {
     Cell** board = new Cell*[rows];
@@ -50,6 +68,53 @@ Cell** initializeBoard(int rows, int cols) {
     return board;
 }
 
+void printCellValue(const Cell& cell) {
+    if (cell.value >= 10 || cell.value <= -10) {
+        cout << " " << cell.operation << " " << cell.value << " ";
+    } else {
+        cout << "  " << cell.operation << cell.value << "  ";
+    }
+}
+
+void displayBoard(Cell** board, int rows, int cols) {
+    for (int j = 0; j < cols; ++j) {
+        cout << "+------";
+    }
+    cout << "+" << endl;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "|";
+
+            if (p1.x == i && p1.y == j) {
+                setColor(PLAYER1_BG, RED_TEXT);
+                cout << "  P1  ";
+            }
+            else if (p2.x == i && p2.y == j) {
+                setColor(PLAYER2_BG, RED_TEXT);
+                cout << "  P2  ";
+            }
+            else if (board[i][j].isVisited) {
+                setColor(board[i][j].bgColor, DEFAULT_TEXT);
+                printCellValue(board[i][j]);
+            }
+            else {
+                setColor(DEFAULT_BG, DEFAULT_TEXT);
+                printCellValue(board[i][j]);
+            }
+
+            resetColor();
+        }
+        cout << "|" << endl;
+
+        for (int j = 0; j < cols; ++j) {
+            cout << "+------";
+        }
+        cout << "+" << endl;
+    }
+}
+
+
 int main() {
     cout << "Enter the number of rows and columns: ";
     cin >> rows >> cols;
@@ -65,7 +130,7 @@ int main() {
     p2.score = 0;
     
     Cell** board = initializeBoard(rows, cols);
-
+    displayBoard(board, rows, cols);
     for (int i = 0; i < rows; i++) {
         delete[] board[i];
     }
